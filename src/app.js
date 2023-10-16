30fa1c166b3c01004b8bfc4e7e08eebb1f7d2ac5
Isaac F <114119376+boxmancoder@users.noreply.github.com>
2023-10-16 01:36:53 +0000

    updated now

--- src/app.js
+++ src/app.js
@@ -2,10 +2,46 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "your", "their", "this", "whos"];
+let adj = [
+  "big",
+  "nice",
+  "handsome",
+  "strong",
+  "beautiful",
+  "bad",
+  "happy",
+  "friendly",
+  "smart",
+  "calm",
+  "tall",
+  "dark",
+  "cute"
+];
+let noun = [
+  "pizza",
+  "house",
+  "florida",
+  "hamburger",
+  "human",
+  "plate",
+  "shoe",
+  "puupy",
+  "kitty",
+  "fire",
+  "flower",
+  "fish",
+  "egg"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateDomain() {
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        console.log(pronoun[p] + adj[a] + noun[n] + ".com");
+      }
+    }
+  }
+}
+
+generateDomain();
